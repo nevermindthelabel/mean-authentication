e3ef889056ae65edb5982af2ed6d4273b6668867
Matt Kilcup <nevermindthelabel@gmail.com>
2019-07-31 22:10:01 -0700

    added models for user account and book

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  isbn: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  }
+});
+
+const Book = mongoose.model('book', BookSchema);
+
+module.exports = Book;
