32f411faa52ec8539502498a8a304932cd164482
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-03 13:00:59 -0700

    added post route for books

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,8 +1,53 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
 const Book = require('../../models/Book');
 
 router.get('/', (req, res) => {
   res.status(200).send('books, yall');
 });
+
+router.post(
+  '/',
+  [
+    check('isbn', 'isbn is required')
+      .not()
+      .isEmpty(),
+    check('title', 'title is obviously required')
+      .not()
+      .isEmpty(),
+    check('author', 'author is obviously required')
+      .not()
+      .isEmpty(),
+    check('publisher', 'publisher is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { isbn, title, author, publisher } = req.body;
+
+    try {
+      let book = await Book.findOne({ isbn });
+      if (book) {
+        return res.status(400).send('book is in database already');
+      }
+
+      let newBook = new Book({
+        isbn,
+        title,
+        author,
+        publisher
+      });
+
+      await newBook.save();
+      res.json({ newBook });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
 
 module.exports = router;
