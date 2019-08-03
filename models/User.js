c54eb7bc0310e9defa58565a98d68ac9d83e0569
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-03 13:00:28 -0700

    changed bcrypt package

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -13,35 +13,25 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-// UserSchema.pre('save', function(next) {
-//   const user = this;
-//   if (this.isModified || this.isNew) {
-//     bcrypt.genSalt(10, function(err, salt) {
-//       if (err) {
-//         return next(err);
-//       }
-//       user.password = hash;
-//       next();
-//     });
-//   } else {
-//     return next();
-//   }
-// });
-
-// UserSchema.methods.comparePassword = function(passw, cb) {
-//   bcrypt.compare(passw.this.password, function(err, isMatch) {
-//     if (err) {
-//       return cb(err);
-//     }
-//     cb(null, isMatch);
-//   });
-// };
+UserSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw.this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model('user', UserSchema);
 
