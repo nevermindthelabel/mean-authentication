5ec41a3740da8ecb39a2ef51ef5b2589f90e3c82
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-03 12:54:27 -0700

    changed route to mongoose.Schema

--- models/Book.js
+++ models/Book.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-
-const BookSchema = new Schema({
+const BookSchema = new mongoose.Schema({
   isbn: {
     type: String,
     required: true
